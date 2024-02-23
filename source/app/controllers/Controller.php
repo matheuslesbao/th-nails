8a7ae8cc4355239ce167693ba806f3ad98252829
matheuslesbao <matheuss.lesbao@gmail.com>
2024-02-23 17:15:09 -0300

    feat: controller e homecontroller

---
+++ source/app/controllers/Controller.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\controllers;
+use League\Plates\Engine;
+abstract class Controller
+{
+    protected function view(string $view, array $data = [])
+    {
+        $pathViews = dirname(__FILE__, 2) . DIRECTORY_SEPARATOR. 'views' ;
+        // Create new Plates instance
+        $templates = new Engine($pathViews);
+
+        // Render a template
+        echo $templates->render($view, $data);
+        
+    }
+}
---
+++ source/app/controllers/HomeController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace app\controllers;
+class HomeController extends Controller
+{
+    public function index(){
+        $this->view('home', []);
+    }
+}
