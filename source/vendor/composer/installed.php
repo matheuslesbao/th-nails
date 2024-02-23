96402e6524f79feb4a53ba82acb2e768222f127e
matheuslesbao <matheuss.lesbao@gmail.com>
2024-02-23 17:12:48 -0300

    ci: composer e docker configurados

---
+++ source/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '89b9ef66f8a0c2fce5159b891369e1930d392a9e',
+        'name' => 'facu/application',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'facu/application' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '89b9ef66f8a0c2fce5159b891369e1930d392a9e',
+            'dev_requirement' => false,
+        ),
+        'league/plates' => array(
+            'pretty_version' => 'v3.5.0',
+            'version' => '3.5.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../league/plates',
+            'aliases' => array(),
+            'reference' => 'a6a3238e46c6e19af7318fdc36bfbe49b0620231',
+            'dev_requirement' => false,
+        ),
+    ),
+);
