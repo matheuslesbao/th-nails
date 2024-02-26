0a4846ce9e2b5cfc3cfac0748dfa0f602e673473
matheuslesbao <matheuss.lesbao@gmail.com>
2024-02-26 00:14:16 -0300

    feat: create section card

--- source/app/views/home.php
+++ source/app/views/home.php
@@ -1,5 +1,39 @@
 <?php $this->layout('principal', ['title' => 'Home', 'style' => 'home']) ?>
-
-<section>
+<main>
     
+<section class="section-cards">
+<h3>Serviços</h3>
+<div class="container-cards">
+  <div class="card">
+    <img src="/assets/img/all.png" alt="Imagem 1">
+    <h2>Pé e Mão</h2>
+    <p>Serviço de completo, incluido esmaltação, corte.</p>
+    <p>R&#36;: 38,00</p>
+  </div>
+  <div class="card">
+    <img src="/assets/img/hand.png"alt="Imagem 2">
+    <h2>Apenas Mão</h2>
+    <p>Sera feito apenas o Mão, incluido esmaltação, corte.</p>
+    <p>R&#36;: 25,00</p>
+  </div>
+  <div class="card">
+    <img src="/assets/img/foot.png" alt="Imagem 3">
+    <h2>Apenas Pé</h2>
+    <p>Sera feito apenas o Pé, incluido esmaltação, corte.</p>
+    <p>R&#36;: 25,00</p>
+  </div>
+  <div class="card">
+    <img src="/assets/img/color.png" alt="Imagem 4">
+    <h2>Esmaltação</h2>
+    <p>Sera feito apenas a esmaltação das unhas.</p>
+    <p>R&#36;: 8,00</p>
+  </div>
+  <div class="card">
+    <img src="/assets/img/corte.png" alt="Imagem 4">
+    <h2>Apenas o corte</h2>
+    <p>Sera feito apenas o corte das unhas.</p>
+    <p>R&#36;: 8,00</p>
+  </div>
+</div>
 </section>
+</main>
