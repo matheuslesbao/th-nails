f230331c39051132cb8c5b71a22b61d1a9fbf084
matheuslesbao <matheuss.lesbao@gmail.com>
2024-02-28 19:45:16 -0300

    style: mais algma alteraçoes no css

--- source/app/views/home.php
+++ source/app/views/home.php
@@ -1,4 +1,4 @@
-<?php $this->layout('principal', ['title' => 'Home', 'style' => 'home']) ?>
+<?php $this->layout('principal', ['title' => 'Thamires Guimarães', 'style' => 'home']) ?>
 <main>
 
   <section class="section-cards">
@@ -9,30 +9,52 @@
         <h2>Pé e Mão</h2>
         <p>Serviço de completo, incluido esmaltação, corte.</p>
         <p>R&#36;: 38,00</p>
+    
+    <button class="whatsapp-button">
+          <span class="whatsapp-icon"></span>
+          <a class="whatsapp-text">Pé e Mão</a>
+        </button>
+
       </div>
       <div class="card">
         <img src="/assets/img/hand.png" alt="Imagem 2">
         <h2>Apenas Mão</h2>
         <p>Sera feito apenas o Mão, incluido esmaltação, corte.</p>
         <p>R&#36;: 25,00</p>
+        <button class="whatsapp-button">
+          <span class="whatsapp-icon"></span>
+          <a class="whatsapp-text">Whatsapp</a>
+        </button>
       </div>
       <div class="card">
         <img src="/assets/img/foot.png" alt="Imagem 3">
         <h2>Apenas Pé</h2>
         <p>Sera feito apenas o Pé, incluido esmaltação, corte.</p>
         <p>R&#36;: 25,00</p>
+        <button class="whatsapp-button">
+          <span class="whatsapp-icon"></span>
+          <a class="whatsapp-text">Whatsapp</a>
+        </button>
       </div>
       <div class="card">
         <img src="/assets/img/color.png" alt="Imagem 4">
         <h2>Esmaltação</h2>
         <p>Sera feito apenas a esmaltação das unhas.</p>
         <p>R&#36;: 8,00</p>
+        <button class="whatsapp-button">
+          <span class="whatsapp-icon"></span>
+          <a class="whatsapp-text">Whatsapp</a>
+        </button>
       </div>
       <div class="card">
         <img src="/assets/img/corte.png" alt="Imagem 4">
         <h2>Apenas o corte</h2>
         <p>Sera feito apenas o corte das unhas.</p>
         <p>R&#36;: 8,00</p>
+        <button class="whatsapp-button">
+          <span class="whatsapp-icon"></span>
+          <a class="whatsapp-text">Whatsapp</a>
+        </button>
       </div>
     </div>
   </section>
@@ -45,7 +67,7 @@
         <img width="356" height="254" src="/assets/img/depo01.jpg" alt="Imagem 1">
       </div>
       <div class="depo">
-        <img width="356" height="254"  src="/assets/img/depo01.jpg" alt="Imagem 1">
+        <img width="356" height="254" src="/assets/img/depo01.jpg" alt="Imagem 1">
       </div>
     </div>
     <div class="container-depo">
@@ -53,7 +75,7 @@
         <img width="356" height="254" src="/assets/img/depo01.jpg" alt="Imagem 1">
       </div>
       <div class="depo">
-        <img width="356" height="254"  src="/assets/img/depo01.jpg" alt="Imagem 1">
+        <img width="356" height="254" src="/assets/img/depo01.jpg" alt="Imagem 1">
       </div>
     </div>
   </section>
